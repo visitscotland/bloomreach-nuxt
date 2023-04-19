f0a3e745fbd0a4936dee26a7db8fa2eabcb3e34d
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2023-04-19 10:42:01 +0100

    Add Grid component

--- app.vue
+++ app.vue
@@ -1,21 +1,25 @@
 <script setup>
 import axios from 'axios';
 import ContentPage from './components/ContentPage.vue';
+import Banner from './components/Banner.vue';
+import Grid from './components/Grid.vue';
 
 // const { VUE_APP_BRXM_ENDPOINT } = useRuntimeConfig();
 // console.log(VUE_APP_BRXM_ENDPOINT)
 
 // Get url of current page.
-const route = useRoute().path === '/' ? '' : useRoute().path;
+const route = useRoute().path;
 
-const endpoint = `https://trial-c11dt83y.bloomreach.io/delivery/site/v1/channels/org-1/pages${route}`;
-// const endpoint = 'https://trial-c11dt83y.bloomreach.io/delivery/site/v1/channels/org-1/pages';
+// const endpoint = `https://trial-c11dt83y.bloomreach.io/delivery/site/v1/channels/trip-planner/pages`;
+// const endpoint = `https://trial-c11dt83y.bloomreach.io/delivery/site/v1/channels/trip-planner/pages?preview-token=a7d348bb-adaf-483b-8733-c2639b39a643`;
+const endpoint = `https://trial-c11dt83y.bloomreach.io/delivery/site/v1/channels/trip-planner/pages?preview-token=47053ca9-2817-448b-b266-b2a79a06e73c`;
 
 const configuration = {
+    path: route,
     endpoint,
     httpClient: axios,
 };
-const mapping = { ContentPage };
+const mapping = { ContentPage, Banner, Grid };
 </script>
     
 <template>
---
+++ components/Grid.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Grid</h1>
+        <pre>{{ document }}</pre>
+        <!-- <pre>{{ br-component.getModels().pagination.$ref }}</pre> -->
+        <!-- <pre>{{ data }}</pre> -->
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{ component: Component, page: Page }>();
+const { component, page } = props;
+
+const documentRef = computed(() => props.component.getModels().document);
+const document = computed(() => documentRef.value && props.page.getContent(documentRef.value));
+// const data = document?.getData()
+
+console.log(document)
+
+</script>
