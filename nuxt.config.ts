6d7248be809212ea79d5759c830c9163db9da7cb
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2023-04-21 16:10:36 +0100

    Remove api endpoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     runtimeConfig: {
-        VUE_APP_BRXM_ENDPOINT: 'https://trial-c11dt83y.bloomreach.io/delivery/site/v1/channels/org-1/pages',
+        VUE_APP_BRXM_ENDPOINT: process.env.VUE_APP_BRXM_ENDPOINT,
     },
     app: {
         head: {
